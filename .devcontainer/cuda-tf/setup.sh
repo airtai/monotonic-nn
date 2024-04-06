65e06006eb739866826ead553853216ad05f9b0e
Davor Runje <davor@airt.ai>
2024-04-06 19:49:33 +0000

    devcontainer fix

--- .devcontainer/cuda-tf/setup.sh
+++ .devcontainer/cuda-tf/setup.sh
@@ -1,13 +1,8 @@
-# install Python packages in virtual environment
-# python3.11 -m venv .venv-3.11
-# source .venv-3.11/bin/activate
-# python -m pip install --upgrade pip
+# update pip
+python -m pip install --upgrade pip
 
-# needed to make sure default python is 3.9 instead of 3.11
-sudo ln -s -f /usr/local/bin/python3.9 /usr/bin/python3
-
-# install packages and make sure we use TF 2.14.1
-pip install -e .[dev] tensorflow==2.14.1
+# install packages
+pip install -e ".[dev]"
 
 # install pre-commit hook if not installed already
 pre-commit install
